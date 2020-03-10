bb6dac59c3b78c238f2c77e07ff2f58138b9f834
JSamuelM <samuel23mena@gmail.com>
2020-03-10 13:25:45 -0600

    Initial commit

---
+++ Controllers/Controller.cs
@@ -0,0 +1,12 @@
+﻿using TournamentApp.Model;
+
+namespace TournamentApp.Controllers
+{
+    interface Controller<T>
+    {
+        Operation<T> addRecord(T o);
+        Operation<T> updateRecord(T o);
+        Operation<T> deleteRecord(T o);
+        Operation<T> getRecords();
+    }
+}
