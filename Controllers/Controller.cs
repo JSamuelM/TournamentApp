2cd15c0271ce03309ba9cc4735f7a7240bf6edab
JSamuelM <samuel23mena@gmail.com>
2020-03-10 19:21:22 -0600

    Prueba de commit

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -4,9 +4,9 @@ namespace TournamentApp.Controllers
 {
     interface Controller<T>
     {
-        Operation<T> addRecord(T o);
-        Operation<T> updateRecord(T o);
-        Operation<T> deleteRecord(T o);
-        Operation<T> getRecords();
+        Operation<T> addRecord(T o); // agregar
+        Operation<T> updateRecord(T o); // actualizar
+        Operation<T> deleteRecord(T o); // eliminar
+        Operation<T> getRecords(); // select
     }
 }
